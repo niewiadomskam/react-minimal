5fa161f39d9f35594c17365f71965acf44e41863
niewiadomskam <51495809+niewiadomskam@users.noreply.github.com>
2019-10-08 22:02:33 +0200

    Lab1Ex3

--- src/MyApp.jsx
+++ src/MyApp.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import AppTitle from './AppTitle'
+import { randomFill } from 'crypto';
 
-const generateArray = (n) => [...Array(n+1).keys()].filter(x=>x>0);
-
+const generateArray = (n) => [...Array(n+1).keys()].filter(x=>x>0); 
+const generateRandomArray =(n) => [...Array(n).keys()].map(() => Math.round(Math.random() * 24)+1);
 
 
 const App = () => (
@@ -10,8 +11,13 @@ const App = () => (
     <AppTitle name="Marta" />
   <h1>Minimal React - niewiadomskam 8b62f7d263cbf41d63725f2dfe718be119fe2917</h1>  
   <p>Bundle size: 57 bytes, Load time of the bundle: 35 ms, Last commit SHA1: f6a9f42152fefba5a78e9929632b278758dcdb70</p>
+  <p>Array with numbers from 1 to 5</p>
   <lu>
     {generateArray(5).map(item=><li>{item}</li>)}
+  </lu>
+  <p>Array with random numbers from 1 to 25</p>
+  <lu>
+    {generateRandomArray(5).map(item=><li>{item}</li>)}
   </lu>
   </div>
 )
